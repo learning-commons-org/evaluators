b9372f5f38c2100adec9ebc4f1acaf9d22a1f1ad
Adnan Hussain <61515575+adnanrhussain@users.noreply.github.com>
2026-05-22 11:54:28 -0700

    build(python-sdk): set up PyPI publishing via release-please (#79)
    * build(python-sdk): set up PyPI publishing via release-please
    
    * chore: sort release-please packages alphabetically

--- sdks/python/src/learning_commons_evaluators/__init__.py
+++ sdks/python/src/learning_commons_evaluators/__init__.py
@@ -1,6 +1,7 @@
 """Learning Commons Evaluators – Python SDK for educational text evaluators."""
 
-from learning_commons_evaluators._version import __description__, __version__
+# Bind __version__ first so consumers reading it during package init never see AttributeError.
+from learning_commons_evaluators.version import __description__, __version__  # noqa: I001
 
 # Config
 from learning_commons_evaluators.config import (
